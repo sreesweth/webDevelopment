33b2a24e8e2efd0c268b03d6baf6bb53c535d9a9
varshinishekar28 <varshinishekar008@gmail.com>
2020-06-13 00:37:09 -0500

    angular

--- Angular-project/book-store/src/app/app.module.ts
+++ Angular-project/book-store/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from '../app/header/header.component';
+import { NavigationComponent } from '../app/navigation/navigation.component';
+import { HomeModule } from './Modules/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
